95fd844db0e0197fb07d27bc5562ec1b5a4baba4
glinkowski <linkowski@hotmail.com>
2017-04-19 13:58:42 -0500

    tested load from snapshot

--- train_depth.sh
+++ train_depth.sh
@@ -3,4 +3,5 @@ set -e
 
 TOOLS=~/Caffe/build/tools
 
-$TOOLS/caffe train --solver=./depth_solver.prototxt $@
+#$TOOLS/caffe train --solver=./depth_solver.prototxt $@
+$TOOLS/caffe train --solver=./depth_solver.prototxt --snapshot snapshots/depth_iter_2614.solverstate $@
