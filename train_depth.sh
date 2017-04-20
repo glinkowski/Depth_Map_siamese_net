508d55e6f91a03d36d90bee28150308be99214a8
glinkowski <linkowski@hotmail.com>
2017-04-19 22:25:55 -0500

    option to resume from iteration snapshot

--- train_depth.sh
+++ train_depth.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env sh
 set -e
 
+# USAGE
+# no command-line arguments will build a new net
+# a number will resume build from iteration xx
+# ie:
+#	~$ ./train_depth.sh 2134
+
+
 TOOLS=~/Caffe/build/tools
 
-#$TOOLS/caffe train --solver=./depth_solver.prototxt $@
-$TOOLS/caffe train --solver=./depth_solver.prototxt --snapshot snapshots/depth_iter_2614.solverstate $@
+if [ $# -gt 0]; then
+	echo "Resuming training from iteration $1"
+	$TOOLS/caffe train --solver=./depth_solver.prototxt \
+		--snapshot snapshots/depth_iter_$1.solverstate
+else
+	GLOG_logtostderr=2 \
+		$TOOLS/caffe train --solver=./depth_solver.prototxt $@
+fi
