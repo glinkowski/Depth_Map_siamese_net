acb58c9779fcd214d97db1ce2e2d9775861eb2cc
glinkowski <linkowski@hotmail.com>
2017-04-20 13:49:59 -0500

    saving terminal output to log file
    Trying to pass '|&' in .sh file, can't. Currently have a hack-ey
    workaround.

--- train_depth.sh
+++ train_depth.sh
@@ -10,11 +10,21 @@ set -e
 
 TOOLS=~/Caffe/build/tools
 
+NOW=$(date +"%m%d%y%T")
+#AMP = %26
+
+echo "$NOW & then"
+
 if [ $# -gt 0]; then
 	echo "Resuming training from iteration $1"
 	$TOOLS/caffe train --solver=./depth_solver.prototxt \
 		--snapshot snapshots/depth_iter_$1.solverstate
 else
-	GLOG_logtostderr=2 \
-		$TOOLS/caffe train --solver=./depth_solver.prototxt $@
+#	GLOG_logtostderr=1 \
+#		$TOOLS/caffe train --solver=./depth_solver.prototxt \
+#		|%26 tee train_depth_2.log $@
+	echo ""
+	echo "Copy and run the following: "
+	echo "$TOOLS/caffe train --solver=./depth_solver.prototxt |& tee logs/train_depth.$NOW.log $@"
+
 fi
