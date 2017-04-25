70872d3738e83b60adc5e8f9e66f49f3291d3af0
glinkowski <linkowski@hotmail.com>
2017-04-25 11:10:58 -0500

    reflects changed file names

--- train_depth_siamese.sh
+++ train_depth_siamese.sh
@@ -17,12 +17,12 @@ echo "$NOW & then"
 
 if [ $# -gt 0]; then
 	echo "Resuming training from iteration $1"
-	$TOOLS/caffe train --solver=./depth_solver.prototxt \
+	$TOOLS/caffe train --solver=./depth_siamese_solver.prototxt \
 		--snapshot snapshots/depth_iter_$1.solverstate \
 		|& tee -i logs/train_depth_$NOW.log $@
 else
 #	GLOG_logtostderr=true \
 #		GLOG_log_dir=logs_glog/single \
-	$TOOLS/caffe train --solver=./depth_solver.prototxt \
+	$TOOLS/caffe train --solver=./depth_siamese_solver.prototxt \
 		|& tee -i logs/train_depth_$NOW.log $@
 fi
